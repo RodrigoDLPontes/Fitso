4517e78b4e7bedc5c9685f59bd1320ac096a8e67
Raj Shrimali <rajshrimali@gmail.com>
2016-09-24 02:50:47 -0400

    add age

--- back/routes/users.js
+++ back/routes/users.js
@@ -21,36 +21,40 @@ router.get('/new', function(req, res, next) {
     var name = req.query.name;
     var email = req.query.email;
     var gender = req.query.gender;
+    var age = req.query.age;
     // test if the email is already in the database
     var uid = hash(email);
     mgclient.connect(curl, function(err, db){
-        var col = db.collections("users");
+        if(err != null){ console.err(err)}
+        var col = db.collection("users");
         col.find({"uid": uid}).toArray(function(err, docs){
             assert.equal(err, null);
             if(docs.length > 0){
                 res.send(
                     {"err": "EMAIL_ALREADY_EXISTS", "res": null}
                 )
+                db.close();
                 return;
             }
             else{
                 var doc = {"uid": uid,
                            "name": name,
                            "gender": gender,
-                           "email": email
+                           "email": email,
+                           "age": age
                         };
                 col.insertOne(doc, function(err, r){
                     assert.equal(null, err)
                     assert.equal(1, r.insertedCount)
                 })
+                db.close();
                 res.send({"err": null, "res": "success"})
             }
         })
-        db.close();
     })
 })
 
-router.get('/groups', function(req, res, next) {
+router.get('/groups', function(req,res, next) {
     // {g, c, s}, = {subgroups ?? }
 })
 module.exports = router;
