1e27e4cf522534b81a420c580d533395e3f3a162
rodrigopontes <rodrigo.dl.pontes@gmail.com>
2016-09-24 05:47:46 -0400

    Created basic sign up activity

---
+++ app/src/main/java/com/fitso/fitso/ActivitiesSelectionActivity.java
@@ -0,0 +1,13 @@
+package com.fitso.fitso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitiesSelectionActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_activities_selection);
+	}
+}
--- app/src/main/java/com/fitso/fitso/SignUpActivity.java
+++ app/src/main/java/com/fitso/fitso/SignUpActivity.java
@@ -1,13 +1,84 @@
 package com.fitso.fitso;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SignUpActivity extends AppCompatActivity {
+
+	EditText nameEditText;
+	EditText emailEditText;
+	EditText passwordEditText;
+	EditText confirmPasswordEditText;
+	EditText ageEditText;
+	RadioGroup genderRadioGroup;
+	Button confirmButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sign_up);
+		nameEditText = (EditText)findViewById(R.id.nameEditText);
+		emailEditText = (EditText)findViewById(R.id.emailEditText);
+		passwordEditText = (EditText)findViewById(R.id.passwordEditText);
+		confirmPasswordEditText = (EditText)findViewById(R.id.confirmPasswordEditText);
+		ageEditText = (EditText)findViewById(R.id.ageEditText);
+		genderRadioGroup = (RadioGroup)findViewById(R.id.genderRadioGroup);
+		confirmButton = (Button)findViewById(R.id.confirmButton);
+	}
+
+	public void confirmButtonClicked(View view) {
+		if(!passwordEditText.getText().toString().equals(confirmPasswordEditText.getText().toString())) {
+			Toast.makeText(this, "Password and confirm password don't match!", Toast.LENGTH_LONG).show();
+			return;
+		}
+		final String name = nameEditText.getText().toString();
+		final String email = emailEditText.getText().toString();
+		final String age = ageEditText.getText().toString();
+		String gender = "m";
+		switch(genderRadioGroup.getCheckedRadioButtonId()) {
+			case R.id.maleRadioButton:
+				gender = "m";
+				break;
+			case R.id.femaleRadioButton:
+				gender = "f";
+				break;
+			case R.id.otherRadioButton:
+				gender = "o";
+				break;
+		}
+		new AsyncTask<String, Void, Void>() {
+			protected Void doInBackground(final String... fields) {
+				try {
+					URL url = new URL("http://128.61.19.249:3000/user/new?name=" + fields[0] + "&email=" + fields[1] + "&age=" + fields[2] + "&gender=" + fields[3]);
+					BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+					String str = in.readLine();
+					in.close();
+					JSONObject json = new JSONObject(str);
+					if(json.getString("err") != null) {
+
+					} else {
+						Toast.makeText(SignUpActivity.this, "Fields invalid!", Toast.LENGTH_LONG).show();
+					}
+				} catch (Exception e) {}
+				return null;
+			}
+		}.execute(name, email, age, gender);
 	}
 }
