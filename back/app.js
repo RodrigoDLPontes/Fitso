da11be5ff4f3254736850b6999af6b9a774ea8c8
Raj Shrimali <rajshrimali@gmail.com>
2016-09-24 13:37:06 -0400

    Initialize matches

--- back/app.js
+++ back/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var match = require('./routes/match')
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/user', users);
+app.use('/match', match)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ back/routes/match.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var url = require('url');
+
+router.get("/new", function(req, res, next){
+
+})
+
+router.get("/join", function(req, res, next){
+
+})
+
+router.get("/browse", function(req, res, next){
+
+})
